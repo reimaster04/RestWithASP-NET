39e47a5145d620c79c98774eebd910ca4c3e994c
unknown <reinaldo@masterdb.net>
2021-06-14 20:22:35 -0300

    Working with four verbs

---
+++ 03_RestwithASPNNET_Classes_Controller_Model/RestwithASPNNET/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestwithASPNNET.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public long FirstName { get; set; }
+        public long LastName { get; set; }
+        public long Address { get; set; }
+        public long Gender { get; set; }
+
+
+
+    }
+}
